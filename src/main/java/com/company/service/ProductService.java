b65dd65dc4f67854e0d567348ed11bf5f7bba150
Emrah Yurtlu <emrahyurtlu@gmail.com>
2020-02-23 00:24:16 +0300

    Request Metodları kaldırıldı.

--- src/main/java/com/company/service/ProductService.java
+++ src/main/java/com/company/service/ProductService.java
@@ -5,10 +5,6 @@ import com.company.dto.Product;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import java.util.List;
 
 @WebService()
@@ -16,26 +12,20 @@ import java.util.List;
 public interface ProductService {
 
     @WebMethod
-    @GET
     Product getById(Integer productId);
 
     @WebMethod
-    @GET
     List<Product> getProducts();
 
     @WebMethod
-    @GET
     List<Product> getByCategory(Integer categoryId);
 
     @WebMethod
-    @POST
     void insert(Product product);
 
     @WebMethod
-    @PUT
     void update(Product product);
 
     @WebMethod
-    @DELETE
     void delete(Integer productId);
 }
